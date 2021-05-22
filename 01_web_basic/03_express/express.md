37f152dfeb95b48201868b585036e3c293df84aa
nao notePC <sagalumaata@gmail.com>
2021-05-22 10:34:47 +0900

    いったん保存

---
+++ 01_web_basic/03_express/express.md
@@ -0,0 +1,4 @@
+
+
+## わかりやすめの参考記事
+https://qiita.com/syumiwohossu/items/f9ee317f31adc3ad387b
--- 01_web_basic/03_express/index.js
+++ 01_web_basic/03_express/index.js
@@ -1,34 +1,56 @@
-/*
 const express = require('express')
 const app = express()
 const port = 8080
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-app.post('/', function (req, res) {
-    res.json(req.body)
-    res.send('Got a POST request')
-  })
-  //https://expressjs.com/ja/api.html#req.body
-  
-  app.listen(port, () => console.log('Example app listening on port 3000!'))
-*/
 
-const express = require('express')
-const app = express()
-const port = 3000
-app.use(express.json())
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
-/*
 app.get('/', (req, res) => {
-  res.json(req.body)
-  res.send('アクセスできました！')
+  //res.send("hello")
+  if(req.query.name) {
+      
+      res.status(200).json({text: req.query.name})
+      
+      //自学
+      //res.status(200).send("200を返します。" + req.query.name)
+      //　curl localhost:8080\?name=user1　
+      // 結果:200を返します。user1
+      //curl -I localhost:8080\?name=user1　
+      //ヘッダー情報(http200含む)のみが返ります。
+      //curl -i localhost:8080\?name=user1
+      //ヘッダー情報とsendのメッセージがかえる
+
+
+    }else{
+      res.status(200).send({text: 'hello world'});
+      //課題1-1問目
+      //curl localhost:8080 -H "Content-Type: application/json"
+      // {text: hello world}
+
+
+    }
+    
 })
-*/
 
 app.post('/', (req, res) => {
-    res.send('Hello World!')
+  //req.is(type)
+  //受信したリクエストが Content-Type ヘッダを持っているか、
+  //また type で指定されたMIMEタイプを持っているかをチェックします。
+  if(req.is('application/json'))
+    {
+    //課題1-2問目 
+    console.log(req.body)
+    res.status(201).send(req.body);
+    //curl localhost:8080 -d '{"name": "hoge"}' -H "Content-Type: application/json"
+    //{"name":"hoge"}%  
+    }else{
+      //課題1-3問目
+      res.status(400).send('エラーです');
+      //curl localhost:8080 -d '{"name": "hoge"}'
+      // HTTPステータス400、エラーが発生するはず
+      return; //ここでreturn しないと処理が終わらない
+    }
+    
   })
 
 
