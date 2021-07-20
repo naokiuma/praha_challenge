98d1fb8c18e7c27a1c32d53a6f5ca346971bbbc4
nao mac <sagalumaata@gmail.com>
2021-07-21 01:37:03 +0900

    jest_first_commit

---
+++ 02_test/2_1_jest_test/jestSample/functions.ts
@@ -0,0 +1,46 @@
+import { NameApiService } from "./nameApiService";
+import { DatabaseMock } from "./util";
+
+
+// 関数の仮引数にも受け入れる型を定義。
+// {}の前にあるstringは関数の返す値の型を定義している。
+
+// function todaysMeal(food: string, energy: number): string {
+//   return `My ${food} has ${energy} calories`
+// }
+
+export const sumOfArray = (numbers: number[]): number => {
+  return numbers.reduce((a: number, b: number): number => a + b);
+};
+
+export const asyncSumOfArray = (numbers: number[]): Promise<number> => {
+  return new Promise((resolve): void => {
+    resolve(sumOfArray(numbers));
+  });
+};
+
+export const asyncSumOfArraySometimesZero = (
+  numbers: number[]
+): Promise<number> => {
+  return new Promise((resolve): void => {
+    try {
+      const database = new DatabaseMock(); // fixme: この関数をテストするには、DatabaseMockの使い方を変える必要がありそう！ヒント：依存性の注入
+      database.save(numbers);
+      resolve(sumOfArray(numbers));
+    } catch (error) {
+      resolve(0);
+    }
+  });
+};
+
+export const getFirstNameThrowIfLong = async (
+  maxNameLength: number
+): Promise<string> => {
+  const nameApiService = new NameApiService(); // fixme: この関数をテストするには、NameApiServiceの使い方を変える必要がありそう！ヒント：依存性の注入
+  const firstName = await nameApiService.getFirstName();
+
+  if (firstName.length > maxNameLength) {
+    throw new Error("first_name too long");
+  }
+  return firstName;
+};
